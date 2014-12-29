1c0998fa274453ff1671de2d0233205393eda9b3
ukgamer <ukgamer0@gmail.com>
2014-12-29 01:25:25 +0000

    Trying to get chatsounds to work

--- chatsounds.py
+++ chatsounds.py
@@ -1,9 +1,7 @@
-# coding=utf8
-from __future__ import unicode_literals
-from willie.module import rule, priority, thread
+from willie.module import commands
 
 import urllib
 
-@willie.module.commands('cs')
+@commands('cs')
 def chatsounds(bot, trigger):
-    bot.say('http://cs.3kelv.in/?s=%s!' % (urllib.quote_plus(trigger.args)))
+    bot.say('http://cs.3kelv.in/?s=%s' % (urllib.quote_plus(trigger.args)))
