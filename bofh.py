6c659458d3767c8d3d0392507ec7c92c5e1d4d04
ukgamer <ukgamer0@gmail.com>
2014-12-30 00:52:01 +0000

    BOFH

---
+++ bofh.py
@@ -0,0 +1,23 @@
+from willie.module import commands
+from willie.config import ConfigurationError
+
+import random
+
+def configure(config):
+    if config.option('Configure BOFH', False):
+        config.add_section('bofh')
+        config.interactive_add(
+            'bofh', 'path',
+            'Path to BOFH text file.',
+            default='/home/hal1320/.willie/bofh.txt'
+        )
+
+def setup(bot):
+    if not bot.config.has_section('bofh'):
+        raise ConfigurationError('BOFH is not configured')
+    if not bot.config.has_option('bofh', 'path'):
+        raise ConfigurationError('BOFH path is not defined')
+
+@commands('bofh')
+def bofh(bot, trigger):
+    bot.say(random.choice(list(open(bot.config.bofh.path))))
