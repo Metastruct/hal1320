02aeb710c7c04cabd8d0b6dca307416693b369e4
ukgamer <ukgamer0@gmail.com>
2015-02-02 20:36:04 +0000

    Spotify test

---
+++ spotify.py
@@ -0,0 +1,26 @@
+from willie import web
+from willie import module
+from willie.module import commands
+import time
+
+import urllib
+
+@rule('.*(play.spotify.com\/track\/)([\w-]+).*')
+def spotify(bot, trigger, found_match=None):
+    match = found_match or trigger
+
+    resp = web.get('https://api.spotify.com/v1/tracks/%s' % match.group(2))
+
+    result = json.loads(resp)
+
+    try:
+        artist = result['artists'][0]['name']
+        title = result['name']
+        album = result['album']['name']
+        duration = result['duration_ms']
+
+        duration_hms = time.strftime('%H:%M:%S', time.gmtime(duration / 1000))
+
+        bot.say('{0} - {1} [{2}] | {3}'.format(artist, title, album, duration_hms))
+    except KeyError:
+        bot.say('Track not found.')
