774a1bc1d0688c7e5e3e38283fd1d9efdbbc4dd7
PotcFdk <PotcFdk@gmx.net>
2015-03-26 03:53:43 +0100

    Let's remove the trailing slash.

--- chatsounds.py
+++ chatsounds.py
@@ -7,4 +7,4 @@ def chatsounds(bot, trigger):
 	if trigger.group(2):
 		bot.say('http://cs.3kv.in/?s=%s' % (urllib.quote(trigger.group(2))))
 	else:
-		bot.say('http://cs.3kv.in/')
+		bot.say('http://cs.3kv.in')
