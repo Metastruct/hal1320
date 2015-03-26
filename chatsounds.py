31544b6c5b2c365d4a4f35685005fa7de08e72e4
PotcFdk <PotcFdk@gmx.net>
2015-03-26 03:52:41 +0100

    Don't error when using .cs without arguments.

--- chatsounds.py
+++ chatsounds.py
@@ -4,4 +4,7 @@ import urllib
 
 @commands('cs')
 def chatsounds(bot, trigger):
-    bot.say('http://cs.3kv.in/?s=%s' % (urllib.quote(trigger.group(2))))
+	if trigger.group(2):
+		bot.say('http://cs.3kv.in/?s=%s' % (urllib.quote(trigger.group(2))))
+	else:
+		bot.say('http://cs.3kv.in/')
