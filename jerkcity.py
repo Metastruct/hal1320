c57374809999ac564c3aa76235dbed47aa983cc1
PotcFdk <PotcFdk@gmx.net>
2015-12-13 15:40:36 +0100

    Fix jerkcity

--- jerkcity.py
+++ jerkcity.py
@@ -1,22 +1,18 @@
 from sopel.module import commands
 from sopel.config import ConfigurationError
+from sopel.config.types import StaticSection, ValidatedAttribute
 
 import random
 
-def configure(config):
-    if config.option('Configure Jerkcity', False):
-        config.add_section('jerkcity')
-        config.interactive_add(
-            'jerkcity', 'path',
-            'Path to Jerkcity text file.',
-            default='/home/hal1320/.sopel/jerkcity.txt'
-        )
+class JerkCitySection(StaticSection):
+    path = ValidatedAttribute('path', default='/home/hal1320/.sopel/jerkcity.txt')
 
 def setup(bot):
-    if not bot.config.has_section('jerkcity'):
-        raise ConfigurationError('Jerkcity is not configured')
-    if not bot.config.has_option('jerkcity', 'path'):
-        raise ConfigurationError('Jerkcity path is not defined')
+    bot.config.define_section('jerkcity', JerkCitySection)
+
+def configure(config):
+    config.define_section('jerkcity', JerkCitySection, validate=False)
+    config.jerkcity.configure_setting('path', 'Path to Jerkcity text file.')
 
 @commands('jerkcity')
 def jerkcity(bot, trigger):
