4e814aa2c06ff96dca4339dd05e1be776b95b79b
Techbot <techbot121@gmail.com>
2019-02-09 16:57:00 +0100

    fix chatsounds for python3+

--- chatsounds.py
+++ chatsounds.py
@@ -5,6 +5,6 @@ import urllib
 @commands('cs')
 def chatsounds(bot, trigger):
 	if trigger.group(2):
-		bot.say('http://cs.3kv.in/?s=%s' % (urllib.quote(trigger.group(2))))
+		bot.say('http://cs.3kv.in/?s=%s' % (urllib.parse.quote(trigger.group(2))))
 	else:
 		bot.say('http://cs.3kv.in')
